d3cf043acc0eebe4dfb6dbe618bbd0c5b00f7a08
Rafael Delboni <rafadelboni@gmail.com>
2023-03-18 17:38:53 -0300

    feat: adds link in home

--- src/main/pages/home/views.cljs
+++ src/main/pages/home/views.cljs
@@ -1,6 +1,13 @@
-(ns main.pages.home.views 
+(ns main.pages.home.views
   (:require [helix.dom :as d]
-            [main.lib :refer [defnc]]))
+            [main.lib :refer [defnc]]
+            [reitit.frontend.easy :as rfe]))
 
 (defnc page []
-  (d/div "Home page"))
+  (d/div
+   "Home page"
+   (d/h1
+    (d/a
+     {:href (rfe/href :routes/organization
+                      {:organization "org.clojure"})}
+     "org.clojure"))))
